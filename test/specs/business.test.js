97fe328ac6a77a2c0f0f587cd3e4e54bfba30325
gmarshd <gordon.marsh@defra.gov.uk>
2025-11-11 19:41:25 +0000

    Initial branch of tests

--- test/specs/business.test.js
+++ test/specs/business.test.js
@@ -7,7 +7,15 @@ import {
   getGetCustomerDetailsGqlQuery,
   getGetCphGqlQuery,
   getGetLandParcelsGqlQuery,
-  getGetLandCoverSummaryGqlQuery
+  getGetLandCoverSummaryGqlQuery,
+  getGetLandCoversGqlQuery,
+  getGetAgreementsGqlQuery,
+  getGetApplicationsGqlQuery,
+  getCreateBusinessGqlMutation,
+  getUpdateBusinessNameGqlMutation,
+  getUpdatePersonGqlMutation,
+  getGetBusinessesCustomersGqlQuery,
+  getGetCustomerBusinessesGqlQuery
 } from '../helpers/graphqlqueries.js'
 import { makePostCall } from '../helpers/apicall.js'
 import { expect } from 'chai'
@@ -97,6 +105,80 @@ describe('Get Land Cover Summary details', () => {
     const jqlVars = '{ "sbi": "114301879"}'
 
     const businessQuery = getGetLandCoverSummaryGqlQuery()
+    const res = await makePostCall(businessQuery, jqlVars)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get Land Covers', () => {
+  it('Land covers should be returned OK', async () => {
+    const jqlVars = '{"sbi":"1111111111","sheetId":"SS6627","parcelId":"5662"}'
+
+    const businessQuery = getGetLandCoversGqlQuery()
+    const res = await makePostCall(businessQuery, jqlVars)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get Agreements', () => {
+  it('Agreements should be returned OK', async () => {
+    const jqlVars = '{ "sbi": "114301879"}'
+
+    const businessQuery = getGetAgreementsGqlQuery()
+    const res = await makePostCall(businessQuery, jqlVars)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get Applications', () => {
+  it('Applications should be returned OK', async () => {
+    const jqlVars = '{ "sbi": "114301879"}'
+
+    const businessQuery = getGetApplicationsGqlQuery()
+    const res = await makePostCall(businessQuery, jqlVars)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Create business', () => {
+  it('A new business can be created', async () => {
+    const businessQuery = getCreateBusinessGqlMutation()
+    const res = await makePostCall(businessQuery)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Update business name', () => {
+  it('An existing businesses name can be updated', async () => {
+    const businessQuery = getUpdateBusinessNameGqlMutation()
+    const res = await makePostCall(businessQuery)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Update Person', () => {
+  it('An existing person can be updated', async () => {
+    const businessQuery = getUpdatePersonGqlMutation()
+    const res = await makePostCall(businessQuery)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get businesses customers', () => {
+  it('Businesses customers should be returned OK', async () => {
+    const jqlVars = '{ "sbi": "114301879"}'
+
+    const businessQuery = getGetBusinessesCustomersGqlQuery()
+    const res = await makePostCall(businessQuery, jqlVars)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get Customers businesses', () => {
+  it('Customers businesses should be returned OK', async () => {
+    const jqlVars = '{ "crn": "1111111100" }'
+
+    const businessQuery = getGetCustomerBusinessesGqlQuery()
     const res = await makePostCall(businessQuery, jqlVars)
     expect(res.status).to.equal(200)
   })
--- test/specs/permissions.test.js
+++ test/specs/permissions.test.js
@@ -1,11 +1,24 @@
-import { getGetPermissionsGroupsGqlQuery } from '../helpers/graphqlqueries.js'
+import {
+  getGetPermissionsGroupsGqlQuery,
+  getGetCustomersBusinessPermissionsGroupsGqlQuery
+} from '../helpers/graphqlqueries.js'
 import { makePostCall } from '../helpers/apicall.js'
 import { expect } from 'chai'
 
-describe('GetPermissionsGroups', () => {
+describe('Get permissions groups', () => {
   it('Permission groups should be returned OK', async () => {
     const permissionsQuery = getGetPermissionsGroupsGqlQuery()
     const res = await makePostCall(permissionsQuery)
+    expect(res.status).to.equal(200)
+  })
+})
+
+describe('Get customers business permission groups', () => {
+  it('Customers business permission groups should be returned OK', async () => {
+    const jqlVars = '{ "sbi": "1000000000", "crn": "1111111100" }'
+
+    const permissionsQuery = getGetCustomersBusinessPermissionsGroupsGqlQuery()
+    const res = await makePostCall(permissionsQuery, jqlVars)
     expect(res.status).to.equal(200)
   })
 })
