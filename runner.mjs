f4552850314d9ecb3c7c12608fff0137fe2bf20c
gmarshd <gordon.marsh@defra.gov.uk>
2025-11-27 18:51:49 +0000

    Update runner.mjs

--- runner.mjs
+++ runner.mjs
@@ -10,6 +10,11 @@ const mocha = new Mocha({
   },
 });
 
+if (process.argv[2]) {
+  process.env.AUTH_TOKEN = process.argv[2].split('=')[1]
+  console.log("The token is: " + process.env.AUTH_TOKEN)
+}
+
 glob.sync("test/**/*.js").forEach((file) => mocha.addFile(file));
 await mocha.loadFilesAsync();
 mocha.run((failures) => process.exit(failures));
